b39eb0019b7253ab80227c1bbe88681d91bd620c
Victor Okechukwu <VicOke360@gmail.com>
2023-01-01 01:00:53 +0100

    updates made

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function PlaySound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
@@ -7,11 +6,10 @@ function PlaySound(e) {
     audio.currentTime = 0
     // audio.currentTime = 0
     key.classList.add("playing")
-    // console.log(e.key);
-    // console.log(e.keyCode);
+    console.log(e);
+    console.log(e.keyCode);
 }
 
-    // console.log(key);
     
 function removeTransition(e) {
     if (e.propertyName !== "transform") return // skips if it isn't a transfom
@@ -22,6 +20,40 @@ function removeTransition(e) {
 }
 
 document.addEventListener("keydown", PlaySound)
-
 const keys = document.querySelectorAll(".key")
 keys.forEach(key => key.addEventListener("transitionend", removeTransition))
+
+
+function PlaySound1(e) {
+    let code;
+    if (e.keyCode) {
+        code = keyCode
+    } else{
+        code = this.dataset.key
+    }
+
+    const audio = document.querySelector(`audio[data-key="${code}"]`)
+    const key = document.querySelector(`.key[data-key="${code}"]`)
+    if (!audio) {
+       return
+    }
+    audio.play()
+    audio.currentTime = 0
+    key.classList.add("playing")
+    console.log(e)
+    console.log(code)
+}
+
+const clickKeys = document.querySelectorAll(".key")
+// clickKeys.forEach(key => {
+//     key.addEventListener("transitionend", removeTransition)
+//     key.addEventListener("click", PlaySound1)
+// })
+
+for (let i = 0, key; key = clickKeys[i]; i++) {
+    key.addEventListener("transitionend", removeTransition)
+    key.addEventListener("click", PlaySound1)
+    
+}
+
+
